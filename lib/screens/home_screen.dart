dcd0c9f5932ddfee0cfe9565f58a29ac5c96e883
김광회 <postelian@gmail.com>
2023-01-04 18:46:19 +0900

    6.6 ListView - optimization

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,8 @@ import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
-
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  // final 을 붙여야 HomeScreen 에 뜬 경고가 사라진다.
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -25,16 +25,32 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      // FutureBuilder: 비동기 함수 실행 결과를 추적하고 실시간 반영해 빌드(랜더)하는 위젯 => 따라서, 플러터는 리액트처럼 state 애용 X
       body: FutureBuilder(
-        // FutureBuilder: 비동기 함수 실행 결과를 추적하고 실시간 반영해 빌드(랜더)하는 위젯 => 따라서, 플러터는 리액트처럼 state 애용 X
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            print('There is Data!');
-            return Text('There is Data!');
+            // ListView: 배열 데이터를 목록 형태로 빌드하는 위젯. 스크롤 기능이 화면에 자동 부여한다.
+            // ListView.builder: 페이징 등 최적화가 가능한 옵션 다수 제공
+            // ListView.separated: builder 메서드와 인수가 거의 동일하고, separatorBuilder 를 추가 필수 인수로 요구한다.
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              // itemBuilder: 사용자가 현재 보려는 아이템만 실시간 빌드하는 옵션
+              itemBuilder: (context, index) {
+                print('built index: $index'); // 실시간 빌드 여부 확인
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              // separatorBuilder: 요소 구분자(스타일 래퍼) 빌더
+              separatorBuilder: (context, index) => SizedBox(
+                width: 20, // 요소 간 간격 20 부여 => 처음과 끝은 자동 생략 처리
+              ),
+            );
           }
-          print('Loading...');
-          return Text('Loading...');
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
