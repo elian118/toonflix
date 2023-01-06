de700433683f3eb1f8de04eed7a7a8fe9ca0127b
김광회 <postelian@gmail.com>
2023-01-06 11:00:27 +0900

    6.17 Favorites

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -68,9 +68,6 @@ class _DetailScreenState extends State<DetailScreen> {
     initPrefs();
   }
 
-  // @override
-  // void d
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
